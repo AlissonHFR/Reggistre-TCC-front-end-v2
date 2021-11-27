79e03a19fd473d893986d2a04e6286d29304a5e5
Alisson Henrique <alissonhenrique98@gmail.com>
2021-11-27 12:02:08 -0300

    feat: create movement table component

---
+++ src/app/pages/movement/list-movements/movement-table/movement-table.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { MovementService } from 'src/app/shared/services/movement.service';
+import { TableService } from 'src/app/shared/services/table.service';
+import { Movement } from '../../../../shared/interfaces/movement.interface';
+
+@Component({
+  selector: 'app-movement-table',
+  templateUrl: './movement-table.component.html',
+  styleUrls: ['./movement-table.component.scss'],
+})
+export class MovementTableComponent implements OnInit {
+  hasLoaded = false;
+  movements: Movement[];
+
+  currentPage = 0;
+  pageCount = 0;
+
+  constructor(
+    private movementService: MovementService,
+    private tableService: TableService
+  ) {}
+
+  ngOnInit(): void {
+    this.movementService.readAll().subscribe({
+      next: (movs) => {
+        this.tableService.entities = movs;
+        this.movements = this.tableService.paginateEntities(this.currentPage);
+        this.pageCount = this.tableService.getPageCount();
+      },
+      error: (e) => console.error(e),
+      complete: () => (this.hasLoaded = true),
+    });
+  }
+
+  transformToBrazilianCurrency(value: number): string {
+    return value.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    });
+  }
+
+  changePage(page: number): void {
+    this.currentPage = page;
+    this.movements = this.tableService.paginateEntities(page);
+  }
+}
